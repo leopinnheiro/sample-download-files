150f14cb2646df5eb65365c61bf2eae27134a491
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2023-01-25 12:32:01 -0300

    Create script for generate sample files

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+
+function createSampleFile(sizeInMB) {
+  const filename = generateName(sizeInMB);
+  console.log(`Generating ${filename}`);
+
+  const fp = fs.openSync(path.join(__dirname, '..', 'sample', filename), "w");
+
+  for (let i = 0; i < sizeInMB; i++) {
+    process.stdout.write(`\rWrite ${i+1}MB`);
+    fs.writeSync(fp, generateOneMb());
+  }
+  console.log('');
+  console.log('Done');
+}
+
+function generateOneMb() {
+  const size = Math.pow(2, 20) * 1;
+  const buffer = Array(size).fill(0x00);
+
+  return new Buffer.from(buffer)
+}
+
+function generateName(sizeInMB) {
+  if (sizeInMB >= 1024) {
+    return `${sizeInMB / 1024}GB.bin`
+  }
+  return `${sizeInMB}MB.bin`
+}
+
+createSampleFile(5);
+createSampleFile(10);
+createSampleFile(50);
+createSampleFile(100);
